f328010d5ba830ded2a81af5e486f4a7e66a6e2e
MJJacobs01 <marthinusjacobs@gmail.com>
2023-06-18 06:31:57 +0200

    Initial commit

---
+++ app/src/main/java/za/co/jacobs/mj/bottomnavbar/BottomNavigation.kt
@@ -0,0 +1,65 @@
+package za.co.jacobs.mj.bottomnavbar
+
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults.colors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.MyNetwork,
+        BottomNavItem.AddPost,
+        BottomNavItem.Notification,
+        BottomNavItem.Jobs
+    )
+
+    BottomAppBar(
+        containerColor = colorResource(id = R.color.teal_200),
+        contentColor = Color.Black
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp
+                    )
+                },
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screen_route,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = colors(
+                    unselectedIconColor = Color.Black.copy(0.4f),
+                    unselectedTextColor = Color.Black.copy(0.4f),
+                    selectedIconColor = Color.Black,
+                    selectedTextColor = Color.Black
+                )
+            )
+        }
+    }
+}
